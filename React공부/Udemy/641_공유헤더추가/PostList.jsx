5b227b21f77ca4b81c303c21a72caec44354d581
ska9611 <143586366+ska9611@users.noreply.github.com>
2024-01-29 18:39:25 +0900

    Add files via upload

---
+++ React공부/Udemy/641_공유헤더추가/PostList.jsx
@@ -0,0 +1,39 @@
+import Post from './Post';
+import classes from './PostList.module.css';
+import NewPost from './NewPost';
+import { useState } from 'react';
+import Modal from './Modal';
+
+function PostList({ isPosting, onStopPosting }) {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      {isPosting ? (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : null}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="운전병123" body="직업은 루미" />
+        <Post author="삼루트아델" body=" 아델" />
+        <Post author="카우욱" body="신궁" />
+      </ul>
+    </>
+  );
+}
+
+export default PostList;
