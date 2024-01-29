dde624e999d3f4a79d7913ff0e1e8c89b4841510
ska9611 <143586366+ska9611@users.noreply.github.com>
2024-01-29 19:16:24 +0900

    Add files via upload

---
+++ React공부/Udemy/643_Sumit버튼/PostList.jsx
@@ -0,0 +1,24 @@
+import Post from './Post';
+import classes from './PostList.module.css';
+import NewPost from './NewPost';
+import { useState } from 'react';
+import Modal from './Modal';
+
+function PostList({ isPosting, onStopPosting }) {
+  return (
+    <>
+      {isPosting ? (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancle={onStopPosting} />
+        </Modal>
+      ) : null}
+      <ul className={classes.posts}>
+        <Post author="운전병123" body="직업은 루미" />
+        <Post author="삼루트아델" body=" 아델" />
+        <Post author="카우욱" body="신궁" />
+      </ul>
+    </>
+  );
+}
+
+export default PostList;
