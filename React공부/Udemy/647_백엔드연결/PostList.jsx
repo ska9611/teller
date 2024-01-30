b132aaa8b09e764853877de1b284476cc2f395f0
ska9611 <143586366+ska9611@users.noreply.github.com>
2024-01-30 09:42:49 +0900

    Create PostList.jsx

---
+++ React공부/Udemy/647_백엔드연결/PostList.jsx
@@ -0,0 +1,44 @@
+import Post from './Post';
+import classes from './PostList.module.css';
+import NewPost from './NewPost';
+import { useState } from 'react';
+import Modal from './Modal';
+
+function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
+  return (
+    <>
+      {isPosting ? (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancle={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      ) : null}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>포스트를 작성하시오</h2>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PostList;
