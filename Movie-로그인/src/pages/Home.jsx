07e73b4be33f87f23b9c525a79e51c7a1fbb525f
ska9611 <143586366+ska9611@users.noreply.github.com>
2024-02-01 20:06:32 +0900

    Add files via upload

---
+++ Movie-로그인/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Login from './Login';
+
+export default function Home() {
+  return (
+    <div className="page-container" style={{ fontSize: '32px' }}>
+      Home
+    </div>
+  );
+}
---
+++ Movie-로그인/src/pages/Search.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div className="page-container" style={{ fontSize: "32px" }}>
+      책 검색페이지
+    </div>
+  );
+}
